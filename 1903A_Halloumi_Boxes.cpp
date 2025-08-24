5f363a7dccc14d52f872cc8e1ce85bfb03ed45c4
Nitesh Sharma <niteshsh34@gmail.com>
2025-08-24 13:27:57 +0530

    Add solution for Halloumi Boxes problem
    Added 1903A_Halloumi_Boxes.cpp

---
+++ 1903A_Halloumi_Boxes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, n, k;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        if (k == 1)
+        {
+            if (is_sorted(a.begin() ,a.end()))
+                cout << "YES" << endl;
+            else
+                cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
